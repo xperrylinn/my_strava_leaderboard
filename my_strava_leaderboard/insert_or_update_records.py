e6b8dc1a6079d2078374442168ca29f95b8670bb
Xavier Linn <xperrylinn@berkeley.edu>
2020-10-13 19:38:37 -0700

    tables created. working on inset update.

---
+++ my_strava_leaderboard/insert_or_update_records.py
@@ -0,0 +1,76 @@
+import os
+import pandas
+import psycopg2
+
+
+def insert_or_update_records(df, db_connection):
+    """
+
+    :param df: dataframe with the following schema:
+        segment id[int],
+        rank[int],
+        total_entries[int],
+        num_attempts[int],
+        read_date[datetime]
+    :type df: pandas.DataFrame
+    :param db_connection: connection to postgresql database
+    :type db_connection: psycopg2 connection
+    :return: None
+    """
+
+    cursor = db_connection.cursor()
+
+    primary_key_set = [record[0] for record in cursor.fetch_all()]
+
+    df['update'] = df['segment_id'].apply(lambda x: x in primary_key_set)
+
+    update_df = df[df['update'] is True]
+    update_records(update_df, db_connection)
+
+    df = df[df['update'] is False]
+    insert_records(df, db_connection)
+
+    return
+
+
+def insert_records(df, db_connection):
+
+    cursor = db_connection.cursor()
+
+    insert_into_command_str = 'INSERT INTO my_leaderboard (segment_id, rank, total_entries, num_attempts, read_date) VALUES '
+    for record in df.to_dict('records'):
+        insert_into_command_str += \
+            '(' + \
+            str(record['segment_id']) + ', ' + \
+            str(record['viewer_rank']) + ', ' + \
+            str(record['total_entries']) + ', ' + \
+            str(record['frequency']) + ', \'' + \
+            str(record['read_date']) + '\'' + \
+            '),'
+    insert_into_command_str = insert_into_command_str[:-1] + ';'
+
+    cursor.execute(insert_into_command_str)
+
+    db_connection.commit()
+
+    return
+
+
+def update_records(df, db_connection):
+
+    cursor = db_connection.cursor()
+
+    for record in df.to_dict('records'):
+        insert_into_command_str = 'UPDATE my_leaderboard SET '
+        insert_into_command_str += \
+            'rank = ' + str(record['viewer_rank']) + ', ' + \
+            'total_entries = ' + str(record['total_entries']) + ', ' + \
+            'num_attempts = ' + str(record['frequency']) + ', ' + \
+            'read_date = \'' + str(record['read_date']) + '\'' + \
+            ' WHERE segment_id = ' + str(record['segment_id']) + ';'
+
+        cursor.execute(insert_into_command_str)
+
+    db_connection.commit()
+
+    return
---
+++ my_strava_leaderboard/test/test_add_or_update_records.py
@@ -0,0 +1,51 @@
+import os
+import pandas
+import psycopg2
+from my_strava_leaderboard.insert_or_update_records import (
+    insert_or_update_records,
+    insert_records,
+    update_records
+)
+
+
+connection = psycopg2.connect(
+    dbname='my_strava_leaderboard',
+    user=os.environ['POSTGRES_USERNAME'],
+    password=os.environ['POSTGRES_PASSWORD']
+)
+
+cursor = connection.cursor()
+
+
+def test_insert_records():
+
+    initial_db_snapshot = pandas.read_csv('./test_data/my_strava_leaderboard_test_data_initial.csv')
+
+    insert_records(initial_db_snapshot, connection)
+
+    assert True
+
+
+def test_update_records():
+
+    new_data = pandas.read_csv('./test_data/my_strava_leaderboard_test_data_new_data.csv')
+
+    update_records(new_data, connection)
+
+    assert True
+
+
+def test_insert_or_update_records():
+
+    initial_db_snapshot = pandas.read_csv('./test_data/my_strava_leaderboard_test_data_initial.csv')
+    new_data = pandas.read_csv('./test_data/my_strava_leaderboard_test_data_new_data.csv')
+    final_db_snapshot = pandas.read_csv('./test_data/my_strava_leaderboard_test_data_final.csv')
+
+    insert_or_update_records(new_data, connection)
+
+    assert True
+
+
+test_update_records()
+
+connection.close()
