d16a7ff60d6752b90e27e63353ab76685c218b09
Xavier Linn <xperrylinn@berkeley.edu>
2020-10-14 21:44:04 -0700

    hello world posgtesql data in index view

--- ebdjango/my_leaderboard/views.py
+++ ebdjango/my_leaderboard/views.py
@@ -1,5 +1,21 @@
 from django.http import HttpResponse
+from sqlalchemy import create_engine
+import pandas
+import os
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+
+    connection_string = 'postgresql://' + \
+                        os.environ['POSTGRES_USERNAME'] + ':' + \
+                        os.environ['POSTGRES_PASSWORD'] + '@' + \
+                        'localhost:5432/my_strava_leaderboard'
+
+    engine = create_engine(connection_string)
+
+    df = pandas.read_sql_query(
+        'SELECT * FROM my_leaderboard;',
+        engine
+    )
+
+    return HttpResponse(df.to_string())
